d5c65334c85555ee97612445eefe5656b5beeaa5
vebcc <vebcc08@gmail.com>
2022-03-27 00:32:31 +0100

    remove webpack encore

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/task')]
+#[Route('/')]
 class TaskController extends AbstractController
 {
     #[Route('/', name: 'app_task_index', methods: ['GET'])]
