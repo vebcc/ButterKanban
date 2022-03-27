ded4ed6bf458f13e44f81100be55dc9f0a5a1947
vebcc <vebcc08@gmail.com>
2022-03-27 09:42:50 +0200

    fix task route

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/')]
+#[Route('/task')]
 class TaskController extends AbstractController
 {
     #[Route('/', name: 'app_task_index', methods: ['GET'])]
