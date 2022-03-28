2f05d4b308cb2bae9764e57824c40ff61d7f8b82
vebcc <vebcc08@gmail.com>
2022-03-29 01:03:36 +0200

    send update task via api

--- public/js/form/group/GroupForm.js
+++ public/js/form/group/GroupForm.js
@@ -19,13 +19,26 @@ $(function () {
     function updateDatabase(event, ui) {
         if (ui.sender) {
             let taskId = ui.item[0].id.split('-')[1];
-            //let id = $(ui.item[0]).html(); // zrobione na razie po nazwie :(
             let oldQueueId = ui.sender[0].id.split('-')[1];
             let newQueueId = event.target.id.split('-')[1];
             console.log("Old queue ID: "+oldQueueId);
             console.log("New queue ID: "+newQueueId);
             console.log("Task ID: "+taskId);
+            sendAjaxRequest("PATCH", "/api/tasks/"+taskId, '{"queue": "api/task_queues/'+newQueueId+'"}', 'application/merge-patch+json');
         }
+    }
+
+    function sendAjaxRequest(method, url, data, contentType) {
+        $.ajax({
+            method: method,
+            url: url,
+            data: data,
+            dataType: 'json',
+            contentType: contentType,
+            success: function(msg) {
+                console.log(msg);
+            }
+        });
     }
 
 });
