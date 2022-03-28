9d9a986ce16e731c23e84ed19d8e88beadc7c9a1
vebcc <vebcc08@gmail.com>
2022-03-28 21:17:20 +0200

    task queue info script

--- public/js/form/group/GroupForm.js
+++ public/js/form/group/GroupForm.js
@@ -13,7 +13,19 @@ $(function () {
     sortable.disableSelection();
 
     sortable.on('sortchange', function (event, ui) {
-        updateHiddenForm(event, ui);
+        updateDatabase(event, ui);
     });
+
+    function updateDatabase(event, ui) {
+        if (ui.sender) {
+            let taskId = ui.item[0].id.split('-')[1];
+            //let id = $(ui.item[0]).html(); // zrobione na razie po nazwie :(
+            let oldQueueId = ui.sender[0].id.split('-')[1];
+            let newQueueId = event.target.id.split('-')[1];
+            console.log("Old queue ID: "+oldQueueId);
+            console.log("New queue ID: "+newQueueId);
+            console.log("Task ID: "+taskId);
+        }
+    }
 
 });
