97a3ee2d58f7284243eb04606fdaec7e78fb366f
vebcc <vebcc08@gmail.com>
2022-03-31 20:21:26 +0200

    working graphql query in javascript

--- public/js/section/home.js
+++ public/js/section/home.js
@@ -1,32 +1,58 @@
 $(document).ready(function () {
+    let superQuery = `query ($id: ID!){
+            task(id: $id){
+                id
+                tittle
+                startData
+                comment
+                taskUsers {
+                    user{
+                        name
+                    }
+                    taskUserType{
+                        name
+                    }
+                }
+                taskComments{
+                    comment
+                }
+            }
+        }`;
 
     $(".card-body").click(function () {
         let modal = $("#TaskModal");
         let id = $(this).data("id");
         console.log(id);
-        //modal.find("#cocktailid").val($(this).data("id"));
-        sendAjaxRequest("GET", "/api/tasks/"+id, '', 'application/merge-patch+json', modal);
-
+        data = parseToSend(superQuery, id, "/api/tasks/")
+        sendAjaxRequest("POST", "/api/graphql", data, 'application/json', modal);
     });
 
-    function sendAjaxRequest(method, url, data, contentType, modal)
-    {
+    function sendAjaxRequest(method, url, data, contentType, modal) {
+        console.log(data);
         $.ajax({
             method: method,
             url: url,
             data: data,
-            dataType: 'json',
             contentType: contentType,
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
                 writeToModal(modal, data);
             }
         });
     }
 
-    function writeToModal(modal, data){
+    function writeToModal(modal, data) {
         modal.find(".taskName").html(data["tittle"]);
         modal.find(".taskComment").html(data["comment"]);
     }
 
+
+    function parseToSend(data, id, repository){
+        return JSON.stringify({
+            query: data,
+            variables: {
+                "id": repository+id
+            }
+        });
+    }
 });
